79a4a7e17c25f809f9661e67975f5fa13f723537
DESKTOP-9M2OV9B\liorh <lhess@playags.com>
2021-10-10 15:37:07 +0300

    sda

--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -64,7 +64,7 @@ namespace ReadExcel
                             continue;
                         }
                     }
-                    
+
                     //write the value to the console
                     if (i % 2 != 0 && xlRange.Cells[j, i] != null && xlRange.Cells[j, i].Value2 != null)
                     {
@@ -98,6 +98,7 @@ namespace ReadExcel
                     totalWeights += "]";
                     totalWeights += "\n";
                 }
-            }  }
+            }
+        }
     }
 }
